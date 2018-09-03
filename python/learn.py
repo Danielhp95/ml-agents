29a4ba7642fe237cd2a218a46f4833bea067add5
Pete Y <pyork31@googlemail.com>
2018-09-03 12:15:58 +0100

    learn.py works with multiprocessing on my machine

--- python/learn.py
+++ python/learn.py
@@ -41,14 +41,18 @@ def run_training(sub_id, run_seed, run_options):
     lesson = int(run_options['--lesson'])
     fast_simulation = not bool(run_options['--slow'])
     no_graphics = run_options['--no-graphics']
-    trainer_config_path = run_options['<trainer-config-path>']
+
+    # Constants
+    # Assumption that this yaml is present in same dir as this file
+    base_path = os.path.dirname(__file__)
+    TRAINER_CONFIG_PATH = os.path.abspath(os.path.join(base_path, "trainer_config.yaml"))
 
     # Create controller and begin training.
     tc = SelfPlayTrainerController(env_path, run_id + '-' + str(sub_id),
                            save_freq, curriculum_file, fast_simulation,
                            load_model, train_model, worker_id + sub_id,
                            keep_checkpoints, lesson, run_seed,
-                           docker_target_name, trainer_config_path, no_graphics)
+                           docker_target_name, TRAINER_CONFIG_PATH, no_graphics)
     tc.start_learning()
 
 
@@ -75,23 +79,25 @@ def main():
     logger = logging.getLogger('mlagents.learn')
     _USAGE = '''
     Usage:
-      learn <trainer-config-path> [options]
+      learn (<env>) [options]
+      learn [options]
       learn --help
+
     Options:
       --env=<file>               Name of the Unity executable [default: None].
       --curriculum=<file>        Curriculum json file for environment [default: None].
       --keep-checkpoints=<n>     How many model checkpoints to keep [default: 5].
       --lesson=<n>               Start learning from this lesson [default: 0].
       --load                     Whether to load the model or randomly initialize [default: False].
-      --run-id=<path>            The directory name for model and summary statistics [default: ppo].
+      --run-id=<path>            The sub-directory name for model and summary statistics [default: ppo].
       --num-runs=<n>             Number of concurrent training sessions [default: 1]. 
       --save-freq=<n>            Frequency at which to save model [default: 50000].
       --seed=<n>                 Random seed used for training [default: -1].
       --slow                     Whether to run the game at training speed [default: False].
       --train                    Whether to train model, or only run inference [default: False].
-      --worker-id=<n>            Number to add to communication port (5005) [default: 0].
-      --docker-target-name=<dt>  Docker volume to store training-specific files [default: Empty].
-      --no-graphics              Whether to run the environment in no-graphics mode [default: False].
+      --worker-id=<n>            Number to add to communication port (5005). Used for multi-environment [default: 0].
+      --docker-target-name=<dt>  Docker Volume to store curriculum, executable and model files [default: Empty].
+      --no-graphics              Whether to run the Unity simulator in no-graphics mode [default: False].
     '''
 
     options = docopt(_USAGE)
